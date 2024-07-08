58b2ceec2df179464d8ef67e75db3230aa8bf176
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2024-07-08 16:50:38 +0200

    recommit

---
+++ src/components/Productions.js
@@ -0,0 +1,37 @@
+// src/components/Productions.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Productions = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const [productions, setProductions] = useState([]);
+
+  useEffect(() => {
+    const fetchProductions = async () => {
+      try {
+        const token = await getAccessTokenSilently();
+        const response = await axios.get('https://quartzib-documents-back-962e7b20e999.herokuapp.com/productions', {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setProductions(response.data);
+      } catch (error) {
+        console.error('Failed to fetch productions', error);
+      }
+    };
+
+    fetchProductions();
+  }, [getAccessTokenSilently]);
+
+  return (
+    <div>
+      {productions.map(production => (
+        <div key={production.id}>
+          <h3>{production.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Productions;
